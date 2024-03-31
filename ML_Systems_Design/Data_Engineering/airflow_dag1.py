cbc6ca52a91730c56a8a55efce97e88d400810bb
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2024-03-31 14:34:30 +0530

    airflow dag-1 for testing

---
+++ ML_Systems_Design/Data_Engineering/airflow_dag1.py
@@ -0,0 +1,35 @@
+
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.providers.docker.operators.docker import DockerOperator
+  
+dag = DAG(
+    'docker_sample',
+    default_args={'retries': 1},
+    schedule_interval=timedelta(minutes=10),
+    start_date=datetime(2021, 1, 1),
+    catchup=False,
+)
+  
+t1 = BashOperator(task_id='print_date', bash_command='date', dag=dag)
+t2 = BashOperator(task_id='sleep', bash_command='sleep 5', retries=3, dag=dag)
+t3 = DockerOperator(
+    docker_url='tcp://localhost:2375',  # Set your docker URL
+    command='/bin/sleep 30',
+    image='centos:latest',
+    network_mode='bridge',
+    task_id='docker_op_tester',
+    dag=dag,
+)
+  
+t4 = BashOperator(
+    task_id='print_hello', 
+    bash_command='echo "hello world!!!"', 
+    dag=dag
+)
+  
+t1 >> t2
+t1 >> t3
+t3 >> t4   
